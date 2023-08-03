94ff64fcc5638b3e21324ee01fdba0a2579966aa
vikash kumar gupta <134148159+vikash-kg@users.noreply.github.com>
2023-08-03 16:48:33 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        // code here
+         vector<int>dist(N,1e9);
+        dist[0]=0;
+        for(int i=0;i<M;i++){
+           
+                int u=edges[i][0];
+                int v=edges[i][1];
+                int w=edges[i][2];
+                
+                dist[v]=min(dist[v],dist[u]+w);
+            
+        }
+        
+        for(int i=0;i<N;i++){
+            if(dist[i]==1e9) dist[i]=-1;
+        }
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for(int i=0; i<m; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
