1a81501d16da475ae04fdfd609c953028e7c07d2
vikash kumar gupta <134148159+vikash-kg@users.noreply.github.com>
2023-08-15 01:53:01 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    Node* reverse(Node* head){
+    if(head== nullptr || head->next == nullptr)
+    return head;
+
+    Node* prev = nullptr, *curr = head, *forward = nullptr;
+    while(curr){
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    return prev;
+}
+    public:
+    Node *compute(Node *head)
+    {
+        // your code goes here
+         head = reverse(head);
+        Node* curr = head;
+        int max = head->data;
+        Node* prev = head;
+        head =head->next;
+
+        while(head){
+            if(head->data >=max){
+                max = head->data;
+                prev = head;
+                head = head->next;
+            }
+            else{
+                prev->next = head->next;
+                head = prev->next;
+            }
+        }
+        head = reverse(curr);
+        return head;
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
